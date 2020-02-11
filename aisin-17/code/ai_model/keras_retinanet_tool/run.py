8a5f849c04feee70abe876ab26f0806b408ff162
John Oldman <johnoldman@MMGF2s-MacBook-Air.local>
2020-02-11 14:26:55 +0700

    tree output mechanism

--- aisin-17/code/ai_model/keras_retinanet_tool/run.py
+++ aisin-17/code/ai_model/keras_retinanet_tool/run.py
@@ -10,6 +10,40 @@ from keras_retinanet.utils.image import read_image_bgr, preprocess_image, resize
 from keras_retinanet.utils.visualization import draw_box, draw_caption
 from keras_retinanet.xmlVOC import make_xml_annotation
 
+
+def tree_output(input_path, output_path, gentool):
+    list_images = os.listdir(input_path)
+    # print(list_images)
+    for img in list_images:
+        # print(img[-4:])
+        if os.path.isdir(os.path.join(input_path, img)):
+            new_input_path = os.path.join(input_path, img)
+            new_output_path = os.path.join(output_path, img)
+            if not os.path.isdir(new_output_path):
+                os.mkdir(new_output_path)
+            tree_output(new_input_path, new_output_path, gentool)
+            print(new_input_path, new_output_path)
+        elif img[-4:] == 'jpeg' or img[-3:] == 'png':
+            img_path = os.path.join(input_path, img)
+            image = read_image_bgr(img_path, slide_window=False)
+            # image1 = read_image_bgr(img_path, slide_window=1)
+            # image2 = read_image_bgr(img_path, slide_window=2)
+            # image3 = read_image_bgr(img_path, slide_window=3)
+
+            image = preprocess_image(image)
+            image, scale = resize_image(image)
+
+            boxes, scores, labels = gentool.tagimage(image, scale)
+            # boxes1, scores1, labels1 = gentool.tagimage(image1)
+            # boxes2, scores2, labels2 = gentool.tagimage(image2)
+            # boxes3, scores3, labels3 = gentool.tagimage(image3)
+            # print(boxes)
+            # print(scores)
+            # print(labels)
+            make_xml_annotation(boxes[0], scores[0], labels[0], img, output_path)
+            # break
+
+
 def main():
     # Read constant
     config = configparser.ConfigParser()
@@ -21,32 +55,8 @@ def main():
     output_xml = config['default']['result_path']
     model_name = config['default']['model_file']
     gentool = GenboudingBox(os.path.join(parent_path, 'models_trained', model_name))
-    list_images = os.listdir(input_img)
-    xml_path = os.path.join(parent_path, output_xml)
-    # print(list_images)
-    for img in list_images:
-        if img[-4:] != 'jpeg' and img[-3:] != 'png':
-            continue
-        img_path = os.path.join(input_img, img)
-        # print(img_path)
-        image = read_image_bgr(img_path, slide_window=False)
-        # image1 = read_image_bgr(img_path, slide_window=1)
-        # image2 = read_image_bgr(img_path, slide_window=2)
-        # image3 = read_image_bgr(img_path, slide_window=3)
-
-        image = preprocess_image(image)
-        image, scale = resize_image(image)
-
-        boxes, scores, labels = gentool.tagimage(image, scale)
-        # boxes1, scores1, labels1 = gentool.tagimage(image1)
-        # boxes2, scores2, labels2 = gentool.tagimage(image2)
-        # boxes3, scores3, labels3 = gentool.tagimage(image3)
-        # print(boxes)
-        # print(scores)
-        # print(labels)
-        make_xml_annotation(boxes[0], scores[0], labels[0], img, xml_path)
-        # break
-
+    # xml_path = os.path.join(parent_path, output_xml)
+    tree_output(input_img, output_xml, gentool)
 
 if __name__ == '__main__':
     main()
